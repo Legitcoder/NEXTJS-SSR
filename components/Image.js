30d063c918b1fa516ddd1fad1f9dcc925a73bcaf
Moin Uddin <U_Moin75@yahoo.com>
2018-07-19 21:47:29 -0400

    NextJS Practice

---
+++ components/Image.js
@@ -0,0 +1,7 @@
+const Image = () => {
+    return (
+        <img src="https://image.shutterstock.com/image-photo/chameleon-furcifer-pardalis-ambolobe-2-260nw-661154734.jpg" />
+    );
+}
+
+export default Image;
